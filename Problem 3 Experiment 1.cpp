938988081110935cc8c7b504175ecbb013092548
Hendrick Brian B. Queza <52022216+hendrickbrianb@users.noreply.github.com>
2019-07-11 22:58:15 +0700

    Update Problem 3 Experiment 1.cpp

--- Problem 3 Experiment 1.cpp
+++ Problem 3 Experiment 1.cpp
@@ -1,3 +1,4 @@
+// preprocessor - it is declared to access iostream, conio.h, and iomanip.
 #include<iostream>
 #include<conio.h>
 #include<iomanip>
@@ -5,6 +6,7 @@ using namespace std;
 
 int main()
 {
+	// double data type is used to store decimal numbers.
 	double averageDailyBalance;
 	double netBalance;
 	double interest;
@@ -12,7 +14,7 @@ int main()
 	short int d1;
 	short int d2;
 	
-	
+	// asks for inputs.
 	cout << "Please Enter Net Balance: " << endl;
 	cin >> netBalance;
 	cout <<"Please Enter Your Payment: " << endl;
@@ -23,8 +25,10 @@ int main()
 	cin >> d1;
 	cout << "Enter How Many Before Billing Cycle: " << endl;
 	cin >> d2;
-	
+	// this is the formula to get the average daily balance.
+	// the average daily balance is netbalance multiplied by in billing cycle minus payment and then multiplied by during billing divided by after billing.
 	averageDailyBalance = netBalance * d1 - payment * d2 / d1;
+	// then the obtained answer is multiplied by 0.0097.
 	interest = (averageDailyBalance*0.0097);
 	cout << "The Interest is: " << "P" <<" "<<showpoint<<setprecision(7)<<interest << endl;
 	
