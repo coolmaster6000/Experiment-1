d9cf91d37fcddd6a4f09cd2226e96f153f40220e
Hendrick Brian B. Queza <52022216+hendrickbrianb@users.noreply.github.com>
2019-07-11 22:52:51 +0700

    Update Problem 2 Experiment 1.cpp

--- Problem 2 Experiment 1.cpp
+++ Problem 2 Experiment 1.cpp
@@ -1,6 +1,9 @@
+// preprocessor - acessed libraries of iostream, conio.h, and iomanip.
+// iomanip - library for manipulation of decimal places
 #include<iostream>
 #include<conio.h>
 #include<iomanip>
+
 using namespace std;
 
 int main()
@@ -9,12 +12,15 @@ int main()
 	int mass;
 	int density;
 	double volume;
-	
+	// asks for mass and density.
 	cout << "Enter Mass: " << " ";
 	cin >> mass;
 	cout << "Enter Density: " <<" ";
 	cin >> density;
+	// mass is divided by density times 1/4 or 0.25.
+	// answer is volume and the units is cubic centimeters
 	volume = 0.25*mass/density;
+	// setprecision - is acessed through iomanip to set the decimal into 2 significant digits.
 	cout << "The Volume Is: " << setprecision(2) << volume << " cm^3" << endl;
 	
 	getch();
